642d9df92d8f287c267d66d90c2ee7fd4862d50a
Jaeyoung Kim <jaeyoung.kim.dev@gmail.com>
2021-03-16 01:02:39 -0600

    Added passport module

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 const bcrypt = require('bcrypt');
+const passport = require('passport');
+
+const initializePassport = require('./passport.config');
+initializePassport(process);
 
 const users = [];
 
@@ -25,9 +29,18 @@ app.get('/register', (req, res) => {
 
 app.post('/register', async (req, res) => {
   try {
-    // const hashedPassword = await bcrypt.hash(req.body.password, 10);
-  } catch {}
-  // req.body.name;
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    users.push({
+      id: Date.now().toString(),
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+    res.redirect('/login');
+  } catch {
+    res.redirect('/register');
+  }
+  console.log(users);
 });
 
 app.listen(3000);
